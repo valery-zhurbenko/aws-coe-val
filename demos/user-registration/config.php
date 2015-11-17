400d38744dfa7a1b589de0be24321ed9f7862eb5
valery-zhurbenko <valery@glide.me>
2015-11-17 15:55:39 +0200

    Update config.php

--- demos/user-registration/config.php
+++ demos/user-registration/config.php
@@ -6,8 +6,8 @@
      $bucket='user-profile-bucket';  #Replace with your Bucket name 
      $tableName = "usr_reg"; #Replace with your Database table name
      $config = array(
-        'key'    => 'AKIAITVNGGCJNTLL7EHA',
-        'secret' => 'Fx+Pjl/B6Tc+Js02qaZSt9GkVvh3q2Fa8yQXSAtL',
+        'key'    => $PARAM1,
+        'secret' => $PARAM2,
         'profile' => 'default',
         'region' => Region::US_EAST_1 #replace with your desired region     
         );
